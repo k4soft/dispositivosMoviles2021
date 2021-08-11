342ce61bd84dc2f176a77cbc0b2fbd629034af50
k4soft <info@k4soft.com>
2021-08-11 18:01:28 -0500

    commit inicial wigets

---
+++ widgets/app/src/main/java/co/com/k4soft/widget/MainActivity.java
@@ -0,0 +1,14 @@
+package co.com.k4soft.widget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

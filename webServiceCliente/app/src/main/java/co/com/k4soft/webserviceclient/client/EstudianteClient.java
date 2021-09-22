0e06c29deb08103f5711ab7b5e4c9d61df8188ed
k4soft <info@k4soft.com>
2021-09-22 18:53:12 -0500

    Se finaliza cliente con retrofit

--- webServiceCliente/app/src/main/java/co/com/k4soft/webserviceclient/client/EstudianteClient.java
+++ webServiceCliente/app/src/main/java/co/com/k4soft/webserviceclient/client/EstudianteClient.java
@@ -1,4 +1,14 @@
 package co.com.k4soft.webserviceclient.client;
 
-public interface EstudianteClient {
+import java.util.List;
+
+import co.com.k4soft.webserviceclient.model.Estudiante;
+import co.com.k4soft.webserviceclient.util.Parametro;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+interface EstudianteClient {
+
+    @GET(Parametro.ESTUDIANTES)
+    Call<List<Estudiante>> getEstudiantes();
 }

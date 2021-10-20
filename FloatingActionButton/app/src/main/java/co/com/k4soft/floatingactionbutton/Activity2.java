dde413085df6a6ced17fffd84d123b8f555b015b
k4soft <info@k4soft.com>
2021-10-20 17:49:04 -0500

    Se crhean nuevos componentes

---
+++ FloatingActionButton/app/src/main/java/co/com/k4soft/floatingactionbutton/Activity2.java
@@ -0,0 +1,14 @@
+package co.com.k4soft.floatingactionbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}

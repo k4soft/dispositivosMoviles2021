938333f351cc6f904aaa8589bd814bf457840e49
k4soft <info@k4soft.com>
2021-10-20 18:35:34 -0500

    se cambia a movable action button

--- FloatingActionButton/app/src/main/java/co/com/k4soft/floatingactionbutton/Activity2.java
+++ FloatingActionButton/app/src/main/java/co/com/k4soft/floatingactionbutton/Activity2.java
@@ -3,6 +3,9 @@ package co.com.k4soft.floatingactionbutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import co.com.k4soft.floatingactionbutton.util.ActionBarUtil;
 
@@ -14,6 +17,30 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
         ActionBarUtil actionBarUtil = new ActionBarUtil(this);
         actionBarUtil.setToolBar("Activity 2", "Esta es una prueba", true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_1, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.goToGuardar) {
+            goToGuardar();
+            return true;
+        } else if  (item.getItemId() == R.id.goToDownload){
+            goToDownload();
+        }
+            return super.onOptionsItemSelected(item);
+    }
+
+    private void goToDownload() {
+        Toast.makeText(this,"pulsaste descargar",Toast.LENGTH_SHORT).show();
+    }
+
+    private void goToGuardar() {
+        Toast.makeText(this,"pulsaste guardar",Toast.LENGTH_SHORT).show();
     }
 
 
